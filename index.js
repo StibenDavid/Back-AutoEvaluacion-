98002ac6dc7cc6851e54da8e126c9b0300f68372
StibenDavid <108556627+StibenDavid@users.noreply.github.com>
2023-06-21 21:05:21 -0500

    Add files via upload
    back de labor creado completamente

--- index.js
+++ index.js
@@ -25,19 +25,23 @@ app.post('/api/login', (req, res) => {
 	const values = [username, password]
 	console.log(values);
 	var connection = mysql.createConnection(credentials)
-	connection.query("SELECT * FROM login WHERE lg_mail = ? AND lg_password = ?", values, (err, result) => {
+	connection.query("SELECT usuario.usr_nombre, usuario.usr_apellido, rol.rol_descripcion, login.usr_identificacion, login.rol_id, lg_mail FROM usuario JOIN login ON usuario.usr_identificacion = login.usr_identificacion JOIN userrol ON usuario.usr_identificacion = userrol.usr_identificacion JOIN rol ON userrol.rol_id = rol.rol_id WHERE lg_mail = ? AND lg_password = ?", values, (err, result) => {
 		if (err) {
 			console.log(err);
 			res.status(500).send(err)
 		} else {
 			if (result.length > 0) {
-				res.status(200).send({
-					"id": result[0].id,
-					"user": result[0].user,
-					"username": result[0].username,
-					"picture": result[0].picture,
-					"isAuth": true
-				})
+				res.status(200).send(
+					{
+						usr_nombre: result[0].usr_nombre,
+						usr_apellido: result[0].usr_apellido,
+						rol_descripcion: result[0].rol_descripcion,
+						usr_identificacion: result[0].usr_identificacion,
+						rol_id: result[0].rol_id,
+						lg_mail: result[0].lg_mail,
+						isAuth: true
+					}
+				)
 			} else {
 				res.status(400).send('Usuario no existe')
 			}
@@ -46,15 +50,17 @@ app.post('/api/login', (req, res) => {
 	connection.end()
 })
 
-app.get('/api/usuarios1', (req, res) => {
+app.post('/api/usuario', (req, res) => {
+	const { id } = req.body
 	var connection = mysql.createConnection(credentials)
-	connection.query('SELECT * FROM usuarios', (err, rows) => {
+	connection.query('SELECT usr_identificacion, usr_nombre, usr_apellido FROM usuario WHERE usr_identificacion = ?', id, (err, result) => {
 		if (err) {
 			res.status(500).send(err)
 		} else {
-			res.status(200).send(rows)
+			res.status(200).send(result)
 		}
 	})
+	connection.end()
 })
 
 app.get('/api/periodos', (req, res) => {
@@ -68,9 +74,9 @@ app.get('/api/periodos', (req, res) => {
 	})
 })
 
-app.get('api/labores',(req,res)=>{
+app.get('/api/periodos2', (req, res) => {
 	var connection = mysql.createConnection(credentials)
-	connection.query('SELECT * FROM labor', (err, rows) => {
+	connection.query('SELECT per_id, per_nombre FROM periodo', (err, rows) => {
 		if (err) {
 			res.status(500).send(err)
 		} else {
@@ -79,50 +85,16 @@ app.get('api/labores',(req,res)=>{
 	})
 })
 
-app.post('/api/labores/eliminar', (req, res) => {
-	const { id } = req.body
-	var connection = mysql.createConnection(credentials)
-	connection.query('DELETE FROM labor WHERE per_id = ?', id, (err, result) => {
-		if (err) {
-			res.status(500).send(err)
-		} else {
-			res.status(200).send({ "status": "success", "message": "Labor Eliminado" })
-		}
-	})
-	connection.end()
-})
-
-app.post('/api/labores/guardar', (req, res) => {
-    const { labor_nombre, labor_horas} = req.body;
-    console.log(req.body);
-    const params = [labor_nombre, labor_horas];
-    var connection = mysql.createConnection(credentials);
-    connection.query('INSERT INTO labor(labor_nombre, labor_horas) VALUES ?', [[params]], (err, result) => {
+app.get('/api/labores', (req, res) => {
+    var connection = mysql.createConnection(credentials)
+    connection.query('SELECT labor.lab_id, labor.lab_nombre, labor.lab_horas, labor.lab_estado, tipolabor.tl_codigo, tipolabor.tl_descripcion FROM labor JOIN tipolabor ON labor.tl_id = tipolabor.tl_id', (err, rows) => {
         if (err) {
-            res.status(500).send(err);
+            res.status(500).send(err)
         } else {
-            res.status(200).send({ "status": "success", "message": "Labor creada" });
+            res.status(200).send(rows)
         }
-        connection.end();
-    });
-});
-
-app.post('/api/labores/editar', (req, res) => {
-	const { labor_id,labor_nombre, labor_horas } = req.body
-	const params = [ labor_nombre, labor_horas]
-	var connection = mysql.createConnection(credentials)
-	connection.query('UPDATE labor SET labor_nombre=?, labor_horas WHERE labor_id = ?', params, (err, result) => {
-		if (err) {
-			res.status(500).send(err)
-		} else {
-			console.log("Modifican ",req.body);
-			console.log("Modifican ",result);
-			res.status(200).send({ "status": "success", "message": "Labor editado" })
-		}
-	})
-	connection.end()
+    })
 })
-	
 
 app.get('/api/autoevaluaciones', (req, res) => {
 	var connection = mysql.createConnection(credentials)
@@ -207,6 +179,22 @@ app.post('/api/autoevaluaciones/eliminar', (req, res) => {
 	connection.end()
 })
 
+app.post('/api/labores/eliminar', (req, res) => {
+	const {id}=req.body
+    var connection = mysql.createConnection(credentials)
+	console.log(id)
+
+    connection.query('DELETE FROM labor WHERE lab_id= ?',id, (err, result) => {
+		
+		if (err) {
+            res.status(500).send(err)
+			console.log("Entroooo")
+        } else {
+            res.status(200).send(result)
+        }
+    })
+})
+
 app.post('/api/guardar', (req, res) => {
 	const { avatar, nombre, planeta } = req.body
 	const params = [[avatar, nombre, planeta]]
@@ -235,6 +223,23 @@ app.post('/api/periodos/guardar', (req, res) => {
         connection.end();
     });
 });
+
+app.post('/api/labores/guardar', (req, res) => {
+    const { lab_nombre,lab_horas, lab_estado, lab_codigo, lab_descripcion } = req.body;
+    console.log(req.body);
+    const params = [lab_nombre,lab_horas, lab_estado, lab_codigo, lab_descripcion];
+    var connection = mysql.createConnection(credentials);
+    connection.query('INSERT INTO labor (lab_nombre,lab_horas, lab_estado, lab_codigo, lab_descripcion) VALUES ?', [[params]], (err, result) => {
+        if (err) {
+            res.status(500).send(err);
+			console.log("entro")
+        } else {
+            res.status(200).send({ "status": "success", "message": "Labor creada" });
+        }
+        connection.end();
+    });
+});
+
 
 app.post('/api/autoevaluaciones/guardar', (req, res) => {
     const { rol_id, usr_identificacion, per_id, lab_id, eva_estado, eva_puntaje, eva_resultado } = req.body;
@@ -280,6 +285,23 @@ app.post('/api/periodos/editar', (req, res) => {
 	})
 	connection.end()
 })
+
+app.post('/api/labores/editar', (req, res) => {
+	const {lab_id,lab_nombre,lab_horas, lab_estado, lab_codigo, lab_descripcion} = req.body
+	const params = [ lab_id,lab_nombre,lab_horas, lab_estado, lab_codigo, lab_descripcion]
+	var connection = mysql.createConnection(credentials)
+	connection.query('UPDATE labor SET lab_nombre=?, lab_horas=?, lab_estado=?,lab_codigo=?, lab_descripcion=? WHERE lab_id = ?', params, (err, result) => {
+		if (err) {
+			res.status(500).send(err)
+		} else {
+			console.log("Modifican ",req.body);
+			console.log("Modifican ",result);
+			res.status(200).send({ "status": "success", "message": "Labor editada" })
+		}
+	})
+	connection.end()
+})
+
 
 app.post('/api/autoevaluaciones/editar', (req, res) => {
 	const { id, rol_id, usr_identificacion, per_id, lab_id, eva_estado, eva_puntaje, eva_resultado} = req.body
